6b1656d0494f9748a471f67004d6eace5d5887bb
Sajid Hossain Chowdhury <127777393+Sajid445@users.noreply.github.com>
2023-04-01 08:10:55 +0530

    Create Clone.sh

---
+++ Clone.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/Sajid445/device-motorola-troika.git -b lineage-20 device/motorola/troika
+git clone https://github.com/LineageOS/android_kernel_motorola_exynos9610.git kernel/motorola/exynos9610
+git clone https://gitlab.com/the-muppets/proprietary_vendor_motorola_troika.git vendor/motorola/troika
+git clone https://gitlab.com/the-muppets/proprietary_vendor_motorola_exynos9610-common.git vendor/motorola/exynos9610-common
+git clone https://github.com/LineageOS/android_device_motorola_exynos9610-common device/motorola/exynos9610-common
+
+git clone https://github.com/LineageOS/android_hardware_motorola.git hardware/motorola
+git clone https://github.com/LineageOS/android_hardware_samsung.git hardware/samsung
+git clone https://github.com/LineageOS/android_hardware_samsung_slsi_fm.git hardware/samsung_slsi/fm
+git clone https://github.com/LineageOS/android_hardware_samsung_slsi_libbt.git hardware/samsung_slsi/libbt
+git clone https://github.com/LineageOS/android_hardware_samsung_slsi_scsc_wifibt_wifi_hal.git hardware/samsung_slsi/scsc_wifibt/wifi_hal
+git clone https://github.com/LineageOS/android_hardware_samsung_slsi_scsc_wifibt_wpa_supplicant_lib.git hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib 
+git clone https://github.com/LineageOS/android_device_samsung_slsi_sepolicy.git device/samsung_slsi/sepolicy
+
